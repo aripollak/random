c4db488d8f7154b182aa1662fdc1c356e4cb4d4d
Ari Pollak <ajp@aripollak.com>
2018-04-17 18:14:55 -0400

    daemonize twitter-notify

--- bin/twitter-notify.js
+++ bin/twitter-notify.js
@@ -3,17 +3,23 @@
  * Sends push notification with the latest tweet using libnotify (GNOME, KDE)
  * To install dependencies:
  *   `npm -g install latest-tweets`
- * Example crontab usage to pop up latest tweet from @tinycarebot every hour during the work day:
- *   `0 9-17/1 * * 1-5 NODE_PATH=/usr/local/lib/node_modules /path/to/twitter-notify.js tinycarebot`
+ * Example usage to pop up latest tweet from @tinycarebot every hour:
+ *   `NODE_PATH=/usr/local/lib/node_modules /path/to/twitter-notify.js tinycarebot 60`
  */
 
-let childProcess = require('child_process');
-let latestTweets = require('latest-tweets');
+const childProcess = require('child_process');
+const latestTweets = require('latest-tweets');
 
-let username = process.argv[2];
+const username = process.argv[2];
 if(!username) throw 'expected Twitter username name as first argument';
-let subtitle = '@' + username + ' (twitter-notify.js)';
+const interval_min = process.argv[3]
 
-latestTweets(username, (err, tweets) => {
-  childProcess.execFile('notify-send', ['-i', 'emblem-favorite', tweets[0].content, subtitle]);
-});
+const subtitle = '@' + username + ' (twitter-notify.js)';
+
+function getLatestTweets() {
+  latestTweets(username, (err, tweets) => {
+    childProcess.execFile('notify-send', ['-i', 'emblem-favorite', tweets[0].content, subtitle]);
+  });
+}
+
+setInterval(getLatestTweets, interval_min * 60 * 1000)
